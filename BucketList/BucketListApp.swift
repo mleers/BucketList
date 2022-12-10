178a3b68c73e10c23d909e21c594eb25216bfd8e
mleers <mleers4@gmail.com>
2022-12-10 12:45:19 -0800

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Mitch on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
